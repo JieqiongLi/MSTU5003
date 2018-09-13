e221c48812722431d7f5f73423ef9921c53aa745
jane363636 <jane363636@gmail.com>
2018-09-13 15:42:39 -0400

    wk2

---
+++ week3 - CSS/test.js
@@ -0,0 +1,3 @@
+<!DOCTYPE html>
+riot.tag2('html', '<head> <meta charset="UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <meta http-equiv="X-UA-Compatible" content="ie=edge"> <title>Document</title> <link rel="stylesheet" href=""> </head> <body> <div class="header"></div> <div id="app1"></div> </body>', '', 'lang="en"', function(opts) {
+});
